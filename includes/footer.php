63f4892782fbca5c0e9ad63f99fb249b90881740
AlixCORBIN <alixc72430@gmail.com>
2025-12-17 10:49:26 +0100

    header + footer

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<script>
+        console.log("Portfolio chargé avec succès.");
+    </script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alix Corbin | Étudiant BUT Info</title>
+    
+    <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@400;700&family=Fredoka+One&family=Cinzel:wght@400;700&family=Roboto:wght@300;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/satisfactory.css">
+    <link rel="stylesheet" href="css/lego.css">
+    <link rel="stylesheet" href="css/chess.css">
+</head>
+<body>
+
+    <nav class="gamemode-nav">
+        <span class="player-name">ALIX CORBIN</span>
+        <div class="nav-links">
+            <a href="#satisfactory">USINE</a>
+            <a href="#lego">CONSTRUCTION</a>
+            <a href="#chess">STRATÉGIE</a>
+        </div>
+    </nav>
